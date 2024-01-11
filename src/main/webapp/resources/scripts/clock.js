2af69cb5de77c684e2a8c3cec60efdcec4eeb702
pmpknu <dangorlyakov@gmail.com>
2024-01-11 17:16:31 +0300

    изменения по дизайну

--- src/main/webapp/resources/scripts/clock.js
+++ src/main/webapp/resources/scripts/clock.js
@@ -1,6 +1,8 @@
 function updateClock() {
     const now = new Date();
 
+    console.log(now)
+
     const hours = now.getHours() % 12;
     const minutes = now.getMinutes();
     const seconds = now.getSeconds();
@@ -9,10 +11,11 @@ function updateClock() {
     const minuteAngle = (360 / 60) * minutes + (360 / 60) * (seconds / 60);
     const secondAngle = (360 / 60) * seconds;
 
-    document.getElementById('hourHand').setAttribute('transform', `rotate(${hourAngle} 100 100)`);
-    document.getElementById('minuteHand').setAttribute('transform', `rotate(${minuteAngle} 100 100)`);
-    document.getElementById('secondHand').setAttribute('transform', `rotate(${secondAngle} 100 100)`);
+    document.getElementById('hourHand').setAttribute('transform', `rotate(${hourAngle} 400 400)`);
+    document.getElementById('minuteHand').setAttribute('transform', `rotate(${minuteAngle} 400 400)`);
+    document.getElementById('secondHand').setAttribute('transform', `rotate(${secondAngle} 400 400)`);
 }
 
 setInterval(updateClock, 12000);
-updateClock();
+
+window.onload = updateClock;
